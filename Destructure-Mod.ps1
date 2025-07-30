5fe4923ef5ebac349ec0b07c3aa161521990ec90
SRochas <SRochas2020@gmail.com>
2025-07-29 22:32:20 -0400

    Add Destructure-Mod Powershell script

---
+++ Destructure-Mod.ps1
@@ -0,0 +1,6 @@
+dotnet tool run Spriggit.Json.Oblivion serialize `
+    --InputPath    ".\mods\Geomancy_Gust_Spell-0_0_1.esp" `
+    --OutputPath   ".\destructured-mods\Geomancy" `
+    --GameRelease  Oblivion `
+    --PackageName  Spriggit.Json.Oblivion `
+    --PackageVersion 0.38.7
